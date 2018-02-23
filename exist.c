01d0433b15fe8af3b902daf1aaf97236448a9506
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-23 16:04:28 +0530

    exist.c

---
+++ exist.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(void)
+{
+	int n[10],m,i,flag=0;
+	scanf("%d %d",&n,&m);
+	int a[10];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==m)
+		{
+			printf("yes");
+			flag=1;
+			break;
+		}
+		
+	}
+	if(flag==0)
+	{
+		printf("no");
+	
+	}
+	return 0;
+}
