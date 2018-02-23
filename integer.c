0cb019d6844c76a3631852306d8bcc3fa181c1a8
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-23 15:50:07 +0530

    integer.c

---
+++ integer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+	int n,count=1;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		n=n/10;
+		if(n!=0)
+		{
+			count=count+1;
+		}
+	}
+	printf("%d",count);
+	return 0;
+}
