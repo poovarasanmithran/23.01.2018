7f72830ff4cc449a52e713ecf3c026ba93a9502f
poovarasanmithran <31285551+poovarasanmithran@users.noreply.github.com>
2018-02-23 16:31:22 +0530

    REPE.C

---
+++ REPE.C
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) 
+{
+	int n[10],m,i,count=0;
+	scanf("%d %d",&n,&m);
+	int a[10];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]==m)
+		{
+			
+			count=count+1;
+		
+		}
+		
+	}
+	printf("%d",count);
+	return 0;
+}
